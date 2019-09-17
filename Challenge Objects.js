61d76173f0045660035e25d2c1213ab1888b0a7a
KESA <49386132+KESA24@users.noreply.github.com>
2019-09-17 16:07:56 +0300

    Add files via upload
    Objects

---
+++ Challenge Objects.js
@@ -0,0 +1,11 @@
+//defining and assigning my variables
+var object = x 
+var output = "";
+var result = "";
+    for(var x= 1; x<=15; x++)
+    {
+        result = x * x;
+        output = output + x + " "+ result +",";
+
+    }
+document.write (output);
